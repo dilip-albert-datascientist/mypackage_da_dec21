33064797a283858415e9a88ef82571f5e640809f
Dilip Albert <Dilip.Albert@gfk.com>
2021-12-06 03:24:07 +0000

    checked tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,15 @@
 # Sample Test passing with nose and pytest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from mypackage_da_dec21.core import MyClass
+
 
 def test_pass():
     assert True, "dummy sample test"
+
+
+def test_get_5():
+    myclass = MyClass()
+    assert myclass.get_5() == 5, "MyClass get_5 returns 5"
